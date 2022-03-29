a44349b9570921f52b6cfaa9acebf8546b0dbb88
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-03-29 11:15:21 +0300

    adaptability

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,15 @@ $(function () {
     slidesToShow: 2,
     dots: true,
     infinite: false,
+    responsive: [
+      {
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      }
+    ]
   });
 
   $('.questions-list__item--active').find('.questions-list__item-text').slideDown();
@@ -28,5 +37,14 @@ $(function () {
       $(this).find('.questions-list__item-text').slideUp(500);
     }
   })
+
+  $('.navbar__menu-btn').on('click', function(){
+    $(this).toggleClass('navbar__menu-btn--active');
+    $('.navbar__menu').toggleClass('navbar__menu--active');
+
+  })
+
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 });
